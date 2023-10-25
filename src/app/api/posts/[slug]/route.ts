1877d9fb62c3075a9b01d43d9695e35df76e04b1
moonseonyeong <seon_moon@naver.com>
2023-10-26 01:12:03 +0900

    [FIX] getPost id에 따라서 참조해오는 table 분기처리

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -4,12 +4,21 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   const params = request.nextUrl.pathname;
   const id = Number(params.split('/')[3]);
+  let post;
 
-  const post = await prisma.posts.findFirst({
-    where: {
-      id: id,
-    },
-  });
+  if (id <= 170) {
+    post = await prisma.old_posts.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  } else {
+    post = await prisma.posts.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  }
 
   return NextResponse.json(post);
 }
